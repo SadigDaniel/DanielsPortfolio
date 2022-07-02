eb2067a4f4625e145145828fc359da3dd5dd6692
Daniel Sadig <65622283+SadigDaniel@users.noreply.github.com>
2022-07-02 15:06:59 -0400

    Revert Changes
    Revert Changes

--- my-udemy-course-turorial-portfolio-master/src/AppLabels.js
+++
@@ -1,8 +0,0 @@
-//import {useTranslation } from "react-i8next";
-
-export default function AppLabels() {
-    //const {t, i8n } = useTranslation();
-    return Object.freeze({
-        ABOUT_ME: "About Me",
-    });
-}
