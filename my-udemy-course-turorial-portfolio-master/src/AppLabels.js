accf0e83ca85d06e8f29ee22d8e5306c786f1528
Daniel Sadig <65622283+SadigDaniel@users.noreply.github.com>
2022-07-02 14:59:33 -0400

    personal portfolio push
    personal portfolio push

---
+++ my-udemy-course-turorial-portfolio-master/src/AppLabels.js
@@ -0,0 +1,8 @@
+//import {useTranslation } from "react-i8next";
+
+export default function AppLabels() {
+    //const {t, i8n } = useTranslation();
+    return Object.freeze({
+        ABOUT_ME: "About Me",
+    });
+}
