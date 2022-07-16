729a9be57a662efd9fae6317708534002661d18e
Daniel Sadig <65622283+SadigDaniel@users.noreply.github.com>
2022-07-16 14:15:25 -0400

    Fixed Header CSS
    Fixed Header CSS

---
+++ my-portfolio/src/helpers/AppLabels.js
@@ -0,0 +1,8 @@
+//import {useTranslation } from "react-i8next";
+
+export default function AppLabels() {
+    //const {t, i8n } = useTranslation();
+    return Object.freeze({
+        ABOUT_ME_INFO: "Hi ! My name is Daniel Sadig, I am currently working towards a Software Engineering degree at Ryerson University  and as a Technical Systems Analyst / Robotic Process Automation engineer at RBC. I am passionate about learning new technologies and further enhancing my skills as a software engineer.",
+    });
+}
